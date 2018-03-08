b8d59ad6c37a57fdf9cd42fb089d4d95fe502f68
Paul Bradshaw <paulonhismobile@gmail.com>
2018-03-08 19:11:41 +0000

    save data

--- scraper.py
+++ scraper.py
@@ -3,6 +3,14 @@ import urllib, json
 url = "https://data.police.uk/api/stops-force?force=west-midlands"
 response = urllib.urlopen(url)
 data = json.loads(response.read())
+
+record = {}
 #print data
+index = 0
 for i in data:
-    print i
+    index = index+1
+    record['index'] = index
+    print i['age_range']
+    record['age'] = i['age_range']
+    record['datetime'] = i['datetime']
+    scraperwiki.sqlite.save(['index'], record)
