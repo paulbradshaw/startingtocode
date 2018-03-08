4841daaf8fb6cbe9f207dfa696ff981d31c1f8aa
Paul Bradshaw <paulonhismobile@gmail.com>
2018-03-08 19:02:05 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,25 +1,8 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import urllib, json
 
-import scraperwiki
-import lxml.html
-#
-# # Read in a page
-html = scraperwiki.scrape("http://foo.com")
-print html
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+url = "https://data.police.uk/api/stops-force?force=west-midlands"
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+#print data
+for i in data:
+    print i
